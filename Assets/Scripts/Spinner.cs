ffd45491919050e68d5b24a75b38ce68b39e8931
SobsForBobs <nontty08@gmail.com>
2026-03-23 14:15:59 -0500

    d

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
+    public Rigidbody rb; // Reference to the Rigidbody component
     public float speed = 90f; // degrees per second
     public Vector3 axis = Vector3.up; // Y-axis by default
 
     void Update()
     {
-        transform.Rotate(axis * speed * Time.deltaTime);
+        rb.AddTorque(axis * speed * Time.deltaTime);
+        //transform.Rotate(axis * speed * Time.deltaTime);
     }
 }
